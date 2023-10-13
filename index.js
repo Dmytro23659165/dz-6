deedffe32c05ae7a2c02b972808ba96dc3a4f921
Dmytro23659165 <dmytrobyrmeha@gmail.com>
2023-10-13 21:47:56 +0300

    dz 1 завд 1

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-// Get the modal
-const modal = document.getElementById("myModal");
+<body>
+  <script src="./js/modal.js"></script>
+</body>
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-const formModal = document.querySelector('#formModal')
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
   }
-}
-
-formModal.addEventListener('submit', e => {
-  e.preventDefault()
-  e.target.elements.fullName.value = ''
-  e.target.elements.email.value = ''
-  e.target.elements.message.value = ''
-  modal.style.display = "none"
-})
+})();
